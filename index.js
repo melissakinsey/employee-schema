fd478b76e18eb377b324110ed65bfb91e148075d
Melissa.Kinsey <kinsey.melissa@gmail.com>
2021-02-04 12:43:47 -0600

    build: Use createConnection() method to join node.js to mySQL.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+// Require express.js and mySQL
+
 const express = require("express");
 
 const mysql = require("mysql");
+
+// Use createConnection() method to join node.js to mySQL
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "#MySQL555",
+    database: "nodemysql"
+})
